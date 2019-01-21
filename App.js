3f6d2b5c054adf0693e60a969ca5f74f1f3a843c
marcelosilvadev <marcelosilva473@hotmail.com>
2019-01-21 01:00:31 -0200

    Modified main file

--- App.js
+++ App.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import { Navigator } from 'react-native-deprecated-custom-components'
+import { Navigator } from 'react-native-deprecated-custom-coimport CenaPrincipal from './src/components/CenaPrincipal';
+mponents'
 import CenaPrincipal from './src/components/CenaPrincipal';
 import CenaClientes from './src/components/CenaClientes'
 
 export default class App extends Component {
   render() {
     return (
-      <Navigator
-        initialRoute={{ id: 'CenaPrincipal' }}
-        renderScene={(route, navigator) => {
-          if (route.id === 'CenaPrincipal') {
-            return (<CenaPrincipal />)
-          }
+      <View>
+        <CenaPrincipal />
+      </View>
+      // <Navigator
+      //   initialRoute={{ id: 'CenaPrincipal' }}
+      //   renderScene={(route, navigator) => {
+      //     if (route.id === 'CenaPrincipal') {
+      //       return (<CenaPrincipal />)
+      //     }
 
-          if (route.id === 'CenaClientes') {
-            return (<CenaClientes />)
-          }
-        }}
-      />
+      //     if (route.id === 'CenaClientes') {
+      //       return (<CenaClientes />)
+      //     }
+      //   }}
+      // />
     );
   }
 }
